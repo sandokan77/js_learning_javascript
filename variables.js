becb091f5e81fbdede3348f463f9090f213e455e
Mona Lisa <chris@smss.org.nz>
2017-11-08 17:15:03 +1300

    lots of stuffs added

--- variables.js
+++ variables.js
@@ -109,7 +109,7 @@ const sam3 = {
     },
 };
 
-//Remarks:
+//Remarks on Trailing Commas in Objects and Arrays
 //C: it's odd that you leave a comma after the last object property:
 //C: 
 const sam1 = {
@@ -147,6 +147,104 @@ s.rating = 3; // no error...success?
 s.rating; //undefined
 
 //Arrays
+/*
+• Array size is not fixed; you can add or remove elements at any time.
+• Arrays are not homogeneous; each individual element can be of any type.
+• Arrays are zero-based. That is, the first element in the array is element 0.
+*/
 
+const a1 = [1, 2, 3, 4]; // array containing numbers
+const a2 = [1, 'two', 3, null]; // array containing mixed types
+const a3 = [ // array on multiple lines
+"E1",
+"E2",
+"E3",
+"E4",
+];
 
+const a4 = [ // array containing objects
+{ name: "Ruby", hardness: 9 },
+{ name: "Diamond", hardness: 10 },
+{ name: "Topaz", hardness: 8 },
+];
+
+const a5 = [ // array containing arrays
+[1, 3, 5],
+[2, 4, 6],
+];
+
+const arr = ['a', 'b', 'c'];
+arr.length; // 3
+
+// get the first element:
+arr[0]; // 'a'
+// the index of the last element in arr is arr.length-1:
+arr[arr.length - 1]; // 'c'
+
+//Replace element in array
+//To overwrite the value at a specific array index, you can simply assign to it:
+const arr = [1, 2, 'c', 4, 5];
+arr[2] = 3; // arr is now [1, 2, 3, 4, 5]
+
+//Dates
+const now = new Date();
+now; // example: Wed Nov 11 2017 16:57:21 GMT+1300 
+
+//Initializing to a specific date
+const halloween = new Date(2016, 9, 31); // note that months are zero-based: 9=October
+
+//Initializing to a specific date and time
+const halloweenParty = new Date(2016, 9, 31, 19, 0); // 19:00 = 7:00 pm
+
+//components of Date object
+halloweenParty.getFullYear(); // 2016
+halloweenParty.getMonth(); // 9
+halloweenParty.getDate(); // 31
+halloweenParty.getDay(); // 1 (Mon; 0=Sun, 1=Mon,...)
+halloweenParty.getHours(); // 19
+halloweenParty.getMinutes(); // 0
+halloweenParty.getSeconds(); // 0
+halloweenParty.getMilliseconds(); // 0
+
+//Regular Expressions (RegExp)
+// simple email recognizer
+const email = /\b[a-z0-9._-]+@[a-z_-]+(?:\.[a-z]+)+\b/;
+
+// US phone number recognizer
+const phone = /(:?\+1)?(:?\(\d{3}\)\s?|\d{3}[\s-]?)\d{3}[\s-]?\d{4}/;
+
+//Map, Set, WeakMap, WeakSet (ES6)
+
+//Data Type Conversion
+//String to number
+const numStr = "33.3";
+const num = Number(numStr); // this creates a number value, *not* an instance of the Number object. If the string can’t be converted to a number, NaN will be returned.
+
+const a = parseInt("16 volts", 10); // the " volts" is ignored, 16 is parsed in base 10
+const b = parseInt("3a", 16); // parse hexadecimal 3a; result is 58
+const c = parseFloat("15.5 kph"); // the " kph" is ignored; parseFloat always assumes base 10
+
+//A Date object can be converted to a number that represents the number of milliseconds since midnight, January 1, 1970, UTC, using its valueOf() method:
+const d = new Date(); // current date
+const ts = d.valueOf(); // numeric value: milliseconds since midnight, January 1, 1970 UTC
+
+//Convert boolean values to 1 (true) or 0 (false)
+const b = true;
+const n = b ? 1 : 0;
+
+//Converting to String
+const n = 33.5;
+n; // 33.5 - a number
+const s = n.toString();
+s; // "33.5" - a string
+
+//Converting Array to String
+const arr = [1, true, "hello"];
+arr.toString(); // "1,true,hello"
+
+//Converting to Boolean
+const n = 0; // "falsy" value
+const b0 = !n; //true !!!!
+const b1 = !!n; // false
+const b2 = Boolean(n); // false
 
