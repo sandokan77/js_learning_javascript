40a36494377ba9aa9e99d072f4c27e3c9e8a44c1
Mona Lisa <chris@smss.org.nz>
2017-11-13 16:08:49 +1300

    3

--- functions.js
+++ functions.js
@@ -194,8 +194,92 @@ function f(x) {
     
         o.func_top_level(); //somehow func_nested() is not executed, to be investigated
 
-    //Function Expressions and Anonymous Functions        
+    //Function Expressions and Anonymous Functions  (there are more to it)  
+    const f2 = function() {return "hello";} //creating the function and assigning it to variable
+
+    //Arrow Notation
+    //benefits:
+    • You can omit the word function.
+    • If the function takes a single argument, you can omit the parentheses.
+    • If the function body is a single expression, you can omit curly braces and the
+    return statement.
+
+    const f1 = function() { return "hello!"; }
+    // OR
+    const f1 = () => "hello!";
+    
+    const f2 = function(name) { return `Hello, ${name}!`; }
+    // OR
+    const f2 = name => `Hello, ${name}!`;
+    
+    const f3 = function(a, b) { return a + b; }
+    // OR
+    const f3 = (a,b) => a + b;
+
+    //call, apply, and bind
+    //1.Call
+    const bruce = { name: "Bruce" };
+    const madeline = { name: "Madeline" };
+    // this function isn't associated with any object, yet it's using 'this'!
+    
+    function greet() {
+        return `Hello, I'm ${this.name}!`;
+    }
+    greet(); // "Hello, I'm !" - 'this' not bound
+    greet.call(bruce); // "Hello, I'm Bruce!" - 'this' bound to 'bruce'
+    greet.call(madeline); // "Hello, I'm Madeline!" - 'this' bound to 'madeline'
     
 
-    
+    function update(birthYear, occupation) {
+        this.birthYear = birthYear;
+        this.occupation = occupation;
+        }
 
+    update.call(bruce, 1949, 'singer');
+        // bruce is now { name: "Bruce", birthYear: 1949,
+        // occupation: "singer" }
+
+    update.call(madeline, 1942, 'actress');
+        // madeline is now { name: "Madeline", birthYear: 1942,
+        // occupation: "actress" }    
+
+    //2.Apply
+    //apply is identical to call except the way it handles function arguments. call takes
+    //arguments directly, just like a normal function. apply takes its arguments as an array
+    update.apply(bruce, [1955, "actor"]);
+    // bruce is now { name: "Bruce", birthYear: 1955,
+    // occupation: "actor" }
+    update.apply(madeline, [1918, "writer"]);
+    // madeline is now { name: "Madeline", birthYear: 1918,
+    // occupation: "writer" }    
+
+    //Note that we simply pass null in for the value of this. That’s because Math.min and Math.max don’t use this at all
+    const arr = [2, 3, -5, 15, 7];
+    Math.min.apply(null, arr); // -5
+    Math.max.apply(null, arr); // 15
+
+    //ES6 "spread" operator
+    const newBruce = [1940, "martial artist"];
+    update.call(bruce, ...newBruce); // equivalent to apply(bruce, newBruce)
+    
+    Math.min(...arr); // -5
+    Math.max(...arr); // 15
+    
+    //3.Bind
+    //allows you to permanently associate a value for this with a function. Imagine we’re
+    //passing our update method around, and we want to make sure that it always gets
+    //called with bruce as the value for this.
+    //Risk: it cannot be used effectively with call, apply, or bind (a second time)
+
+    const updateBruce = update.bind(bruce);
+    updateBruce(1904, "actor");
+    // bruce is now { name: "Bruce", birthYear: 1904, occupation: "actor" }
+    updateBruce.call(madeline, 1274, "king");
+    // bruce is now { name: "Bruce", birthYear: 1274, occupation: "king" };
+    // madeline is unchanged
+    
+    //bind with parameters provided
+    const updateBruce1949 = update.bind(bruce, 1949);
+    updateBruce1949("singer, songwriter");
+    // bruce is now { name: "Bruce", birthYear: 1949,
+    // occupation: "singer, songwriter" }
